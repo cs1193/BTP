fc6a49ed82cebbcfef213dec91fc8908d7dd34cc
Chandresh <cs1193@gmail.com>
2014-01-05 14:48:45 +0530

    Just Started Working on Shit

---
+++ application/config/twitter.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['user'] = "data.chand@gmail.com";
+$config['pass'] = "brave_busters";
+
+$config['twitter_consumer_token']		= 'QXgyXKxLS0FHqYko137Kxw';
+$config['twitter_consumer_secret']		= 'nbIqoQLo1HhEZne0x23aSogJrjKpCOMiBgQcgYxQ';
+$config['twitter_access_token']			= '430724573-tnA1spkneDRIyMSadfrH08x59QsiRD00biyz1UeA'; // Optional
+$config['twitter_access_secret']		= 'udSikDxxQbppRVNarsAV2FuXnLYdnxnn5sr6pj5F6ksP7'; // Optional
+
+/* End of file twitter.php */
+/* Location: ./application/config/twitter.php */
